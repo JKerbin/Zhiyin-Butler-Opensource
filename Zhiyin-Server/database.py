23427f19e0cba2f1f32bcd5a9a3a887618d4b3ca
JKerbin <huang_jiaxiang@foxmail.com>
2024-06-06 13:39:03 +0800

    beta0.1.2更新，修复服务器和MySQL数据库断连的bug

--- Zhiyin-Server/database.py
+++ Zhiyin-Server/database.py
@@ -1,32 +1,30 @@
 import re
-
 import pymysql
-
 from utils import *
+
+"""
+# 邀请码表 #
+CREATE TABLE invitation_codes (
+    invitation VARCHAR(255)
+);
++------------+--------------+------+-----+---------+-------+
+| Field      | Type         | Null | Key | Default | Extra |
++------------+--------------+------+-----+---------+-------+
+| invitation | varchar(255) | YES  |     | NULL    |       |
++------------+--------------+------+-----+---------+-------+
+# 用户mac表 #
+CREATE TABLE allowed_users (
+    mac_address VARCHAR(17)
+);
++-------------+-------------+------+-----+---------+-------+
+| Field       | Type        | Null | Key | Default | Extra |
++-------------+-------------+------+-----+---------+-------+
+| mac_address | varchar(17) | YES  | UNI | NULL    |       |
++-------------+-------------+------+-----+---------+-------+
+"""
 
 
 class Database:
-    """
-    # 邀请码表 #
-    CREATE TABLE invitation_codes (
-        invitation VARCHAR(255)
-    );
-    +------------+--------------+------+-----+---------+-------+
-    | Field      | Type         | Null | Key | Default | Extra |
-    +------------+--------------+------+-----+---------+-------+
-    | invitation | varchar(255) | YES  |     | NULL    |       |
-    +------------+--------------+------+-----+---------+-------+
-    # 用户mac表 #
-    CREATE TABLE allowed_users (
-        mac_address VARCHAR(17)
-    );
-    +-------------+-------------+------+-----+---------+-------+
-    | Field       | Type        | Null | Key | Default | Extra |
-    +-------------+-------------+------+-----+---------+-------+
-    | mac_address | varchar(17) | YES  | UNI | NULL    |       |
-    +-------------+-------------+------+-----+---------+-------+
-    """
-
     def __init__(self, user, pw, db_name, host='127.0.0.1', port=3306):
         """
         :param user: 用户名
@@ -35,33 +33,46 @@ class Database:
         :param host: 地址（缺省值127.0.0.1）
         :param port: 端口（缺省值3306）
         """
+        self.user = user
+        self.pw = pw
+        self.db_name = db_name
+        self.host = host
+        self.port = port
+        self.__connect_to_db()
+
+    def __connect_to_db(self):
         try:
             self.__c = 'DATABASE'
             self.mas = Massage(self.__c)
-            self.__db = pymysql.connect(host=host, user=user, passwd=pw, port=port)
+            self.__db = pymysql.connect(
+                host=self.host,
+                user=self.user,
+                passwd=self.pw,
+                port=self.port,
+                db=self.db_name
+            )
             self.__cursor = self.__db.cursor()
-            self.__cursor.execute('USE ' + db_name)
-            self.mas.info(f'建立与MySQL_{db_name}的连接，数据库用户为{user}')
+            self.mas.info(f'建立与MySQL_{self.db_name}的连接，数据库用户为{self.user}')
         except Exception as e:
             self.mas.error(e)
 
+    def __ping_db(self):
+        try:
+            self.__db.ping(reconnect=True)
+        except pymysql.MySQLError as e:
+            self.mas.warning(e)
+            self.__connect_to_db()
+
     @staticmethod
     def __filter_mac_addresses(mac_list):
-        """
-        去除非标准17位（加上连接符）的mac地址（例如虚拟网络适配器mac地址）
-        """
         def is_valid_mac(mac):
-            # Regular expression to match a valid MAC address
             valid_mac_pattern = re.compile(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
             return bool(valid_mac_pattern.match(mac))
 
         return [mac for mac in mac_list if is_valid_mac(mac)]
 
     def add_invitation(self, invitations):
-        """
-        生成新的邀请码
-        :param invitations: 新增邀请码
-        """
+        self.__ping_db()
         try:
             for invitation in invitations:
                 sql = "INSERT INTO invitation_codes (invitation) VALUES (%s)"
@@ -73,21 +84,13 @@ class Database:
             self.mas.error(e)
 
     def user_verification(self, mac_list):
-        """
-        用户验证
-        :param mac_list: 用户mac地址列表
-        """
+        self.__ping_db()
         mac_list = self.__filter_mac_addresses(mac_list)
         try:
-            # mac_list 是包含多个 MAC 地址的列表
-            # 动态生成 SQL 语句中的占位符
             placeholders = ', '.join(['%s'] * len(mac_list))
             sql = f"SELECT * FROM allowed_users WHERE mac_address IN ({placeholders})"
-
-            # 执行查询
             self.__cursor.execute(sql, mac_list)
             result = self.__cursor.fetchone()
-
             if result:
                 self.mas.info(f'用户{mac_list}已注册')
                 return 'success'
@@ -99,11 +102,7 @@ class Database:
             return 'fail'
 
     def user_activation(self, mac_list, invitation):
-        """
-        用户激活
-        :param mac_list: 用户mac地址列表
-        :param invitation: 用户提供的邀请码
-        """
+        self.__ping_db()
         mac_list = self.__filter_mac_addresses(mac_list)
         state = self.user_verification(mac_list)
         if state == 'success':
@@ -115,7 +114,6 @@ class Database:
                 self.__cursor.execute(sql, (invitation,))
                 result = self.__cursor.fetchone()
                 if result:
-                    # 动态生成 SQL 语句中的占位符，添加用户mac地址并删除对应邀请码
                     placeholders = ', '.join(['(%s)'] * len(mac_list))
                     sql = f"INSERT INTO allowed_users (mac_address) VALUES {placeholders} " \
                           f"ON DUPLICATE KEY UPDATE mac_address=VALUES(mac_address)"
